a5195471fe451481a793600fa680f7b9a896bd3a
xudingjun3131 <xudingjun3131@16.com>
2017-10-13 17:12:18 +0800

    Signed-off-by: xudingjun3131 <xudingjun3131@16.com> xudingjun

--- local_settings.py
+++ local_settings.py
@@ -6,9 +6,9 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES.update({
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'opencmdb',                      # Or path to database file if using sqlite3.
-        'USER': 'opencmdb',                     # Not used with sqlite3.
-        'PASSWORD': 'opencmdb',                  # Not used with sqlite3.
+        'NAME': 'yunwei',                      # Or path to database file if using sqlite3.
+        'USER': 'yunwei',                     # Not used with sqlite3.
+        'PASSWORD': 'yunwei',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3307',                      # Set to empty string for default. Not used with sqlite3.
         'OPTIONS':{'charset': 'utf8', }
